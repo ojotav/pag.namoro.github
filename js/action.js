81ed4f97cd3dfff316611bf83cb80fefcb45ba8a
ojotav <132151398+ojotav@users.noreply.github.com>
2023-05-20 01:29:51 -0300

    Upload do Site de Pedido de Namoro

---
+++ js/action.js
@@ -0,0 +1,11 @@
+var moveButton = document.querySelector("button:nth-child(2)");
+
+var correctlyButton = document.querySelector("button:first-child");
+
+moveButton.addEventListener("click", function () {
+  var vertical = Math.floor(Math.random() * 400);
+  var horizontal = Math.floor(Math.random() * 400);
+  moveButton.style.position = "absolute";
+  moveButton.style.left = vertical + "px";
+  moveButton.style.top = horizontal + "px";
+});
